774e38e97a120a355202f368858d3dde4ba5b78a
UZBase <sharofiddinov.07@mail.ru>
2021-12-12 13:41:20 +0300

    v 1.0.2

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,44 @@
 window.addEventListener('load', (e) => {
     const dino = document.getElementById('dino')
     const cactus = document.getElementById('cactus')
+    const start = document.getElementById('start')
 
-    document.addEventListener('keydown', (event) => {
-        jump()
+    start.addEventListener('click', (event) => {
+        if (start.innerHTML == "Stop") {
+            start.innerHTML = "Start";
+            cactus.classList.remove('cacON')
+
+
+        } else {
+            start.innerHTML = "Stop";
+            cactus.classList.add('cacON')
+
+            document.addEventListener('keydown', (event) => {
+                jump()
+            })
+
+
+            function jump() {
+                if (dino.classList != "jump") {
+                    dino.classList.add('jump')
+                }
+                setTimeout(() => {
+                    dino.classList.remove("jump")
+                }, 300);
+            }
+
+            let isAlive = setInterval(function () {
+                let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
+                let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+
+                if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+                    alert("Game over.")
+                }
+            }, 10)
+        }
     })
 
-    function jump() {
-        if (dino.classList != "jump") {
-            dino.classList.add('jump')
-        }
-        setTimeout(() => {
-            dino.classList.remove("jump")
-        }, 300);
-    }
-    let isAlive = setInterval(function(){
-        let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
-        let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
 
-        if(cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
-            alert("Game over.")
-        }
-    },10)
 
 
 
