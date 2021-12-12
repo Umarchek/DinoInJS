a4faac1ead85f037d15a87c951bbf39582d6d26b
UZBase <sharofiddinov.07@mail.ru>
2021-12-12 13:22:32 +0300

    v 1.0.0 end

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,34 @@
-window.addEventListener('load',(e)=>{
-    
+window.addEventListener('load', (e) => {
+    const dino = document.getElementById('dino')
+    const cactus = document.getElementById('cactus')
+
+    document.addEventListener('keydown', (event) => {
+        jump()
+    })
+
+    function jump() {
+        if (dino.classList != "jump") {
+            dino.classList.add('jump')
+        }
+        setTimeout(() => {
+            dino.classList.remove("jump")
+        }, 300);
+    }
+    let isAlive = setInterval(function(){
+        let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"))
+        let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"))
+
+        if(cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
+            alert("Game over.")
+        }
+    },10)
+
+
+
+
+
+
+
+
+
 })
